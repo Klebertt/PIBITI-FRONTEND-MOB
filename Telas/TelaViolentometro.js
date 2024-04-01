5194035a4a4d48c630d8bb14db9571b09d099648
Klebertt <kjab2@aluno.ifal.edu.br>
2024-04-01 14:13:30 -0300

    fix: Correção do espaçamento do texto na view "thirdView"

--- Telas/TelaViolentometro.js
+++ Telas/TelaViolentometro.js
@@ -14,8 +14,20 @@ const TelaViolentometro = () => {
       <View style={styles.thirdView}>
         <Text style={styles.textosTerceiraView}>É possível estarem em um relacionamento abusivo e não perceber. Acredite, é mais comum do que se imagina! Essa aqui é uma ferramenta que vai te ajudar a reconhecer quando você ou alguém próximo estiver em perigo.</Text>
         <View style={styles.retangularView}>
-        <Text style={styles.textosEsquerda}>FIQUE ATENTA!</Text>
-        <Text style={styles.textosEsquerda}>A violência está presente. Essas atitudes não são legais e não vão diminuir!</Text>
+          <View style={styles.colunaEsquerda}>
+            <Text style={styles.textosEsquerdaTopo}>FIQUE ATENTA!</Text>
+            <Text style={styles.textosEsquerdaTopo}>A violência está presente. Essas atitudes não são      legais e não vão diminuir!</Text>
+          </View>
+          <View style={styles.colunaDireita}>
+            <Text style={styles.textosEsquerdaBaixo}>Culpar                Ameaçar
+Controlar
+Proibir
+Humilhar em público
+Chatangear
+Diminuir a autoestima
+Ciúmes excessivos
+Mentir/enganar</Text>
+          </View>
         </View>
         <View style={styles.retangularView2}></View>
         <View style={styles.retangularView3}></View>
@@ -60,46 +72,62 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   retangularView: {
-    height: config.deviceHeight*0.24,
+    height: config.deviceHeight * 0.24,
     backgroundColor: 'white',
     borderRadius: 10,
     margin: 10,
-    marginLeft: config.deviceWidth*0.05,
-    marginRight: config.deviceWidth*0.05,
-    marginTop: config.deviceHeight*0.02,
+    marginLeft: config.deviceWidth * 0.05,
+    marginRight: config.deviceWidth * 0.05,
+    marginTop: config.deviceHeight * 0.02,
     backgroundColor: "#FEB000",
     shadowColor: 'black',
     shadowOpacity: 1,
     elevation: 6,
+    flexDirection: 'row', // Organiza os elementos em linha
   },
   retangularView2: {
-    height: config.deviceWidth*0.4,
+    height: config.deviceWidth * 0.4,
     backgroundColor: 'white',
     borderRadius: 10,
     margin: 10,
-    marginLeft: config.deviceWidth*0.05,
-    marginRight: config.deviceWidth*0.05,
-    marginTop: config.deviceHeight*0.02,
+    marginLeft: config.deviceWidth * 0.05,
+    marginRight: config.deviceWidth * 0.05,
+    marginTop: config.deviceHeight * 0.02,
     backgroundColor: "#FE6A00",
     shadowColor: 'black',
     shadowOpacity: 1,
     elevation: 6,
   },
   retangularView3: {
-    height: config.deviceWidth*0.4,
+    height: config.deviceWidth * 0.4,
     backgroundColor: 'white',
     borderRadius: 10,
     margin: 10,
-    marginLeft: config.deviceWidth*0.05,
-    marginRight: config.deviceWidth*0.05,
-    marginTop: config.deviceHeight*0.02,
+    marginLeft: config.deviceWidth * 0.05,
+    marginRight: config.deviceWidth * 0.05,
+    marginTop: config.deviceHeight * 0.02,
     backgroundColor: "#F91B0E",
     shadowColor: 'black',
     shadowOpacity: 1,
     elevation: 6,
   },
-  textosEsquerda:{
-    textAlign: 'left'
+  colunaEsquerda: {
+    flex: 1, // Expande para ocupar todo o espaço disponível na coluna
+    justifyContent: 'center', // Centraliza verticalmente
+    paddingLeft: 20, // Espaçamento à esquerda
+  },
+  colunaDireita: {
+    flex: 1, // Expande para ocupar todo o espaço disponível na coluna
+    justifyContent: 'center', // Centraliza verticalmente
+    paddingRight: 20, // Espaçamento à direita
+  },
+  textosEsquerdaTopo: {
+    textAlign: 'left',
+    fontWeight: 'bold',
+  },
+  textosEsquerdaBaixo: {
+    textAlign: 'left',
+    marginTop: 10, // Espaçamento superior
   },
 });
 
